b26581607ef62f60b16c443622866446dde4141f
Skyearn <SignUp@viuk.top>
2026-07-31 19:16:06 +0800

    Initial Commit

---
+++ MouseDisplayLock/MouseDisplayLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  MouseDisplayLockApp.swift
+//  MouseDisplayLock
+//
+//  Created by Al on 2026/7/31.
+//
+
+import SwiftUI
+
+@main
+struct MouseDisplayLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MouseDisplayLockTests/MouseDisplayLockTests.swift
@@ -0,0 +1,19 @@
+//
+//  MouseDisplayLockTests.swift
+//  MouseDisplayLockTests
+//
+//  Created by Al on 2026/7/31.
+//
+
+import Testing
+@testable import MouseDisplayLock
+
+struct MouseDisplayLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
